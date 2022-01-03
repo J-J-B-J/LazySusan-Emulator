f321c6ed4f45dfea97a93aa7beb2799b6b29976a
J <joshua.lea@icloud.com>
2022-01-03 20:05:38 +1100

    V1.2
    You can now "Enable" or "Disable" table objects. This means that when you have taken an item off your lazy Susan, you can tell the program so. The program will then do nothing when that item is asked for!

--- lazy suzan.py
+++ lazy suzan.py
@@ -6,15 +6,18 @@ def delay(length):
 current_position = 0
 modifier = 0
 objects = {}
+all_objects = {}
 
 def print_current_position():
     print("The table is now in position " + str(current_position) + ".")
 print_current_position()
 
 class table_object:
-    def __init__(self, name, pos):
+    def __init__(self, name, pos, use):
         self.name = name
         self.position = pos
+        self.use = use
+        all_objects[self.name] = self
 
     def goto(self, modifier):
         global current_position
@@ -37,22 +40,30 @@ class table_object:
         print_current_position()
         delay(1000)
 
+    def used(self):
+        self.use = True
+        objects[self.name] = self
+        
+    def unused(self):
+        self.use = False
+        del objects[self.name]
 
-cutlery = table_object("Cutlery", 0)
-sauce = table_object("Sauce", 90)
-desert = table_object("Desert", 270)
 
-
-objects = {
-    "Cutlery": cutlery,
-    "Sauce": sauce,
-    "Desert": desert,
-}
+cutlery = table_object("Cutlery", 0, False)
+tom_sauce = table_object("Tomato Sauce", 0, False)
+bbq_sauce = table_object("BBQ Sauce", 0, False)
+chilli_sauce = table_object("Sweet Chilli Sauce", 0, False)
+salad = table_object("Salad", 0, False)
+salt = table_object("Salt", 0, False)
+pepper = table_object("Pepper", 0, False)
+bible = table_object("Bible", 0, False)
+olive_oil = table_object("Olive Oil", 0, False)
 
 while True:
     action = str(input("Enter action: "))
+
     
-    if action == "goto":
+    if action == "Goto":
         person = str(input("Who is this? "))
         
         if person == "A":
@@ -72,3 +83,32 @@ while True:
         for obj, identifier in objects.items():
             if str(obj) == goto:
                 identifier.goto(modifier)
+
+
+    if action == "Remove":
+        remove = input("Item to remove? ")
+        
+        for obj, identifier in objects.items():
+            if str(obj) == remove:
+                remove = identifier
+        remove.unused()
+    
+
+    if action == "Add":
+        add = input("Item to add? ")
+        
+        for obj, identifier in all_objects.items():
+            if str(obj) == add:
+                identifier.used()
+
+
+    if action == "List":
+        print("\n\nAll items:\n")
+        print(all_objects)
+        print("\n\nUsed items:\n")
+        print(objects)
+        print("\n\n")
+
+
+
+
