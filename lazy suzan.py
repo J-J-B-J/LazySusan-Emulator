b0815a5be7b7e668d5dff01ce94ace61e8f6b632
J <joshua.lea@icloud.com>
2022-01-04 09:35:28 +1100

    V1.3
    The new version turns the code for getting the modifier for each person into a function, for ease of reading. It also checks for validity whenever it asks for user input, then prints an appropriate error message depending on what was wrong with the input. Finally, the new version also prints the items that are on the table whenever there is a change to them.

--- lazy suzan.py
+++ lazy suzan.py
@@ -8,9 +8,42 @@ modifier = 0
 objects = {}
 all_objects = {}
 
+def list_items_on_table():
+    print("\nItems on table:                           *UPDATED*")
+    for obj, identifier in objects.items():
+        print(" - " + obj)
+    print("\n")
+
 def print_current_position():
     print("The table is now in position " + str(current_position) + ".")
 print_current_position()
+
+def invalid_answer(user_input, error):
+    print("Sorry, but '" + user_input + "' " + error)
+
+def get_modifier():
+    first_time = True
+    modifier = "X"
+    person = ""
+    
+    while modifier == "X":
+        if first_time == False:
+            invalid_answer(person)
+        person = str(input("Who is this? "))
+        first_time = False
+        if person == "A":
+            modifier = int(0)
+            
+        if person == "B":
+            modifier = int(90)
+                
+        if person == "C":
+            modifier = int(180)
+
+        if person == "D":
+            modifier = int(270)
+
+    return modifier
 
 class table_object:
     def __init__(self, name, pos, use):
@@ -43,10 +76,12 @@ class table_object:
     def used(self):
         self.use = True
         objects[self.name] = self
+        list_items_on_table()
         
     def unused(self):
         self.use = False
         del objects[self.name]
+        list_items_on_table()
 
 
 cutlery = table_object("Cutlery", 0, False)
@@ -64,50 +99,57 @@ while True:
 
     
     if action == "Goto":
-        person = str(input("Who is this? "))
-        
-        if person == "A":
-            modifier = int(0)
-            
-        if person == "B":
-            modifier = int(90)
-            
-        if person == "C":
-            modifier = int(180)
-
-        if person == "D":
-            modifier = int(270)
-
+        miodifier = get_modifier()
         goto = input("Where to? ")
-
+        valid_answer_found = False
         for obj, identifier in objects.items():
             if str(obj) == goto:
                 identifier.goto(modifier)
+                valid_answer_found = True
+        if valid_answer_found == False:
+            invalid_answer(goto, "wasn't on the table. The table did nothing.")
 
 
-    if action == "Remove":
+    elif action == "Remove":
         remove = input("Item to remove? ")
-        
+        found_item = False
         for obj, identifier in objects.items():
             if str(obj) == remove:
                 remove = identifier
-        remove.unused()
+                found_item = True
+        if found_item == True:
+            remove.unused()
+        else:
+            invalid_answer(remove, "wasn't on the table.")
+            list_items_on_table()
     
 
-    if action == "Add":
+    elif action == "Add":
         add = input("Item to add? ")
         
+        found_item = False
         for obj, identifier in all_objects.items():
             if str(obj) == add:
-                identifier.used()
+                correct_identifier = identifier
+                found_item = True
+
+        item_on_table = False
+        for obj, identifier in objects.items():
+            if str(obj) == add:
+                item_on_table = True
+                
+        if found_item == False:
+            invalid_answer(add, "dosen't exist.")
+            list_items_on_table()
+        elif item_on_table == True:
+            invalid_answer(add, "is already on the table.")
+            list_items_on_table()
+        else:
+            correct_identifier.used()
 
 
-    if action == "List":
-        print("\n\nAll items:\n")
-        print(all_objects)
-        print("\n\nUsed items:\n")
-        print(objects)
-        print("\n\n")
+    else:
+        invalid_answer(action)
 
 
 
